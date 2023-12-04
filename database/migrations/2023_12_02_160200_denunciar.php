695d8a47593813d0183f981722b4c7d6e3f3e4c9
cainanaraujo <cainandropa1301@outlook.com>
2023-12-04 14:18:50 -0300

    fix: denunciar

--- database/migrations/2023_12_02_160200_denunciar.php
+++ database/migrations/2023_12_02_160200_denunciar.php
@@ -4,9 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDenunciasTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('denunciar', function (Blueprint $table) {
             $table->id();
@@ -21,8 +24,11 @@ class CreateDenunciasTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('denunciar');
     }
-}
+};
